89b29502ef52bcba65dd29aa4c6054eb00b91a3d
LuckyY <xianglucky88@gmail.com>
2026-08-24 13:40:08 +0800

    green2：含 Bug 初始验收快照

---
+++ internal/api/middleware.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+	"time"
+)
+
+func requestLog(logger *slog.Logger, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		started := time.Now()
+		next.ServeHTTP(w, r)
+		logger.Info("request", "method", r.Method, "path", r.URL.Path, "duration", time.Since(started))
+	})
+}
+
+func recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if value := recover(); value != nil {
+				_ = debug.Stack()
+				writeError(w, http.StatusInternalServerError, "internal server error")
+			}
+		}()
+		nextHandler := next
+		nextHandler.ServeHTTP(w, r)
+	})
+}
